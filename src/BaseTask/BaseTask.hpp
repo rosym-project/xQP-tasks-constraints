cf15ec9f68d96c065df663a3da19ad0b98d8a389
pouya-moh <other.pouya@gmail.com>
2019-11-19 10:14:08 +0100

    add class memebers and functions to BaseTask
    Currently there are three TODOs
    1. should the regularization factor be here?
    2. should the floating base boolean be declared here?
    3. add the math to doxygen comments

--- src/BaseTask/BaseTask.hpp
+++ src/BaseTask/BaseTask.hpp
@@ -9,6 +9,7 @@
 #define BASETASK_H
 
 #include <Eigen/Core>
+#include <string>
 
 /**
  * @brief The BaseTask class
@@ -23,7 +24,98 @@ public:
     BaseTask();
     virtual ~BaseTask();
 
-private:
+    /**
+     * @brief update updates the Jacobian et al. with current system state
+     * @param q the system state (e.g., robot configuration)
+     * @return True if all fine, false otherwise
+     */
+    virtual bool update(const Eigen::VectorXd & q) = 0;
+
+    /**
+     * @brief compute_cost_function
+     * @return True of all fine, flase otherwise
+     * Computes the cost function that is the core of the QP problem. The
+     * implementation depends on the nature of derived task.
+     */
+    virtual bool compute_cost_function() = 0;
+
+
+    /**
+     * @brief getConstraintMatrix
+     * @return Constant reference to this->constraint_matrix
+     */
+    const Eigen::MatrixXd & getConstraintMatrix() const {
+        return this->constraint_matrix;
+    }
+
+    /**
+     * @brief getConstraintBounds
+     * @return Constant reference to this->constraint_bounds
+     */
+    const Eigen::VectorXd & getConstraintBounds() const {
+        return this->constraint_bounds;
+    }
+
+    /**
+     * @brief getTaskSize
+     * @return The task size, AKA, the number of rows of the Jacobian
+     */
+    int getTaskSize() {
+        return this->task_size;
+    }
+
+
+protected:
+
+    /**
+     * @brief task_name An identifier for task
+     */
+    std::string task_name;
+
+    /**
+     * @brief J The Jacobian of the task
+     * This is used to compute the Hessian matrix for the QP solver. For a
+     * velocity task for instance, the sub class will use this to compute
+     * J^T.J. Other task will use this accordingly.
+     */
+    Eigen::MatrixXd J;
+
+    /**
+     * @brief e The error of the task
+     * This vector expresses the error function for a given task. For isntance,
+     * in velocity control e=x_dot.
+     */
+    Eigen::VectorXd e;
+
+    /**
+     * @brief e_feedback The error feedback
+     * Is a positive error feedback gain matrix.
+     */
+    Eigen::MatrixXd feedback_gain;
+
+    /**
+     * @brief constraint_matrix
+     * This matrix together with constraint_bound define a ploytope which bound
+     * the solution of Jq-e such that it is constraint_matrix*q<contraint_bound
+     */
+    Eigen::MatrixXd constraint_matrix;
+
+    /**
+     * @brief constraint_bounds
+     * This vector together with constraint_matarix define a ploytope that bound
+     * the solution of Jx-e such that it is constraint_matrix*q<contraint_bound
+     */
+    Eigen::VectorXd constraint_bounds;
+
+    /**
+     * @brief task_size The dimension of the task
+     * AKA, the number of rows in the Jacobian matrix
+     */
+    int task_size;
+
+    //TODO should the regularization factor be here?
+    //TODO should the floating base boolean be declared here?
+
 };
 
 
