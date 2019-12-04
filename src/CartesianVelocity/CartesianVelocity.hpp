3a1badf0d3ccd162ab30470ebfce52eb73f78594
pouya-moh <other.pouya@gmail.com>
2019-12-04 16:13:00 +0100

    add cartesian task bare bone

---
+++ src/CartesianVelocity/CartesianVelocity.hpp
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Author: Pouya Mohammadi
+ *
+ */
+
+
+#ifndef CARTESIANVELOCITY_H
+#define CARTESIANVELOCITY_H
+
+
+
+#endif /* CARTESIANVELOCITY_H */
