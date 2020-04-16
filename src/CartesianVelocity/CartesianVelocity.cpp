4d4297d45aa8d7f522fc73c464aaa34138cd61cb
pouya-moh <other.pouya@gmail.com>
2020-04-16 12:20:23 +0200

    update the task constructor

--- src/CartesianVelocity/CartesianVelocity.cpp
+++ src/CartesianVelocity/CartesianVelocity.cpp
@@ -6,9 +6,30 @@
 
 #include "CartesianVelocity.hpp"
 
-CartesianVelocity::CartesianVelocity(std::string name, int hessian){
+//CartesianVelocity::CartesianVelocity(std::string name, Eigen::VectorXd &home_conf, int hessian){
+//    this->task_name = name;
+//    this->hessian_type = hessian;
+////    this->model = KinDynModelFactory::initialize()
+//}
+
+CartesianVelocity::CartesianVelocity(std::string name,
+                                     int hessian,
+                                     std::string urdf_path,
+                                     std::string base_link,
+                                     std::string ee_link,
+                                     Eigen::VectorXd &home_conf,
+                                     bool floating_base,
+                                     bool parse_urdf_verbose,
+                                     int kin_dyn_solver){
     this->task_name = name;
     this->hessian_type = hessian;
+    this->model = KinDynModelFactory::initialize(urdf_path,
+                                                 base_link,
+                                                 ee_link,
+                                                 floating_base,
+                                                 parse_urdf_verbose,
+                                                 kin_dyn_solver);
+
 }
 
 bool CartesianVelocity::update(const Eigen::VectorXd &q) {
--- src/CartesianVelocity/CartesianVelocity.hpp
+++ src/CartesianVelocity/CartesianVelocity.hpp
@@ -14,12 +14,21 @@ class CartesianVelocity : public BaseTask
 {
 public:
     CartesianVelocity();
-    CartesianVelocity(std::string name, int hessian);
+    CartesianVelocity(std::string name,
+                      int hessian,
+                      std::string urdf_path,
+                      std::string base_link,
+                      std::string ee_link,
+                      Eigen::VectorXd &home_conf,
+                      bool floating_base,
+                      bool parse_urdf_verbose = false,
+                      int kin_dyn_solver = KinDynSolvers::RBDL);
     virtual ~CartesianVelocity();
 
     bool compute_cost_function() override;
     bool do_sanity_check() override;
     bool update(const Eigen::VectorXd &q) override;
+
 };
 
 // TODO implement floating based stuff
